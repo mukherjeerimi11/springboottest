9bd19e1d7b9850a195478f29f14ccc7b421786ae
Sanando1989 <57519773+Sanando1989@users.noreply.github.com>
2020-06-12 13:15:05 +0530

    test

---
+++ src/main/java/com/example/demo/RestController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+	
+	@GetMapping("health")
+	public String health(){
+		return "Running Successfully";
+	}
+
+}
